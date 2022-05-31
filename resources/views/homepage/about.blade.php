2637ba28fb51680c4638b11e71efe8554cc93237
safira123-pixel <safiraistifarini@gmail.com>
2022-05-31 21:15:19 +0700

    Edit Halaman Contact Us

--- resources/views/homepage/about.blade.php
+++ resources/views/homepage/about.blade.php
@@ -4,6 +4,12 @@
   <div class="row">
     <div class="col">
       <h1>Tentang Kami</h1>
+      <h2> You Can Contact Us At Here </h2>
+      <a href="tel:+6281370587320">+62-813-705-873-20</a>
+      <br>
+      <a href="mailto:safiraistifarini@gmail.com" itemprop="email">RentACar@gmail.com</a>
+      <br>
+      <a href="https://www.instagram.com/kurozuki_kun/" itemprop="instagram">Instagram</a>
     </div>
   </div>
 </div>
