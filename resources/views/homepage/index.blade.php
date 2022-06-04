031d93ffb1d838c1342447941c1503e588d3436b
safira123-pixel <safiraistifarini@gmail.com>
2022-06-04 16:16:20 +0700

    update homepage

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.template')
 @section('content')
+
 <div class="container">
   <!-- carousel -->
   <div class="row">
@@ -28,5 +29,86 @@
     </div>
   </div>
   <!-- end carousel -->
+  <!-- produk Terbaru-->
+  <div class="row mt-4">
+    <div class="col col-md-12 col-sm-12 mb-4">
+      <h2 class="text-center">List Of Car Units</h2>
+    </div>
+    <!-- produk pertama -->
+    <div class="col-md-4">
+      <div class="card mb-4 shadow-sm">
+        <!-- <a href="{{ URL::to('produk/satu') }}"></a> -->
+          <img src="{{asset('images/Honda.jpg') }}" alt="foto produk" class="card-img-top">
+        <div class="card-body">
+          <!-- <a href="{{ URL::to('produk/satu') }}" class="text-decoration-none"></a> -->
+            <p class="card-text">
+              <b>HONDA N7X</b>
+            </p>
+          <div class="row mt-4">
+            <div class="col">
+              <!-- <button class="btn btn-light">
+                <i class="far fa-heart"></i>
+              </button> -->
+            </div>
+            <div class="col-auto">
+              <p>
+                Rp. 900.000,00 / hari
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- produk kedua -->
+    <div class="col-md-4">
+      <div class="card mb-4 shadow-sm">
+        <!-- <a href="{{ URL::to('produk/dua') }}"></a> -->
+          <img src="{{asset('images/HondaBr-v.jpg') }}" alt="foto produk" class="card-img-top">
+        <div class="card-body">
+          <!-- <a href="{{ URL::to('produk/dua') }}" class="text-decoration-none"></a> -->
+            <p class="card-text">
+              <b>HONDA BR-V</b>
+            </p>
+          <div class="row mt-4">
+            <div class="col">
+              <!-- <button class="btn btn-light">
+                <i class="far fa-heart"></i>
+              </button> -->
+            </div>
+            <div class="col-auto">
+              <p>
+                Rp. 800.000,00 / hari
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- produk ketiga -->
+    <div class="col-md-4">
+      <div class="card mb-4 shadow-sm">
+        <!-- <a href="{{ URL::to('produk/tiga') }}"></a> -->
+          <img src="{{asset('images/AvanzaToyota.jpeg') }}" alt="foto produk" class="card-img-top">
+        <div class="card-body">
+          <!-- <a href="{{ URL::to('produk/tiga') }}" class="text-decoration-none"> </a>-->
+            <p class="card-text">
+              <b>AVANZA TOYOTA</b>
+            </p>
+          <div class="row mt-4">
+            <div class="col">
+              <!-- <button class="btn btn-light">
+              </button> -->
+            </div>
+            <div class="col-auto">
+              <p>
+                Rp. 750.000,00 / hari
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end produk terbaru -->
 </div>
 @endsection
