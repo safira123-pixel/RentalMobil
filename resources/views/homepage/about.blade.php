f99157e669e29aad24cead954c86489283bb1523
safira123-pixel <safiraistifarini@gmail.com>
2022-06-04 15:37:24 +0700

    update about us dan footer

--- resources/views/homepage/about.blade.php
+++ resources/views/homepage/about.blade.php
@@ -3,13 +3,62 @@
 <div class="container">
   <div class="row">
     <div class="col">
-      <h1>Tentang Kami</h1>
-      <h2> You Can Contact Us At Here </h2>
-      <a href="tel:+6281370587320">+62-813-705-873-20</a>
-      <br>
-      <a href="mailto:safiraistifarini@gmail.com" itemprop="email">RentACar@gmail.com</a>
-      <br>
-      <a href="https://www.instagram.com/kurozuki_kun/" itemprop="instagram">Instagram</a>
+      <h1> CV. NEED A CAR </h1>
+      <h2>TRUSTED MALANG CAR RENTAL</h2>
+      <h3>The most complete fleet of car rental services in Pekanbaru, high year output. Immediately enjoy the best service from us.Call us at 081 370 587 320</h3>
+
+      <!-- kategori produk -->
+  <div class="row mt-4">
+    <div class="col col-md-12 col-sm-12 mb-4">
+      <!-- <h2 class="text-center">Kategori Produk</h2> -->
+    </div>
+    <!-- kategori pertama -->
+    <div class="col-md-4">
+      <div class="card mb-4 shadow-sm">
+        <!-- <a href="{{ URL::to('kategori/satu') }}">
+          <img src="{{asset('images/slide1.jpg') }}" alt="foto kategori" class="card-img-top"> -->
+        </a>
+        <div class="card-body">
+          <!-- <a href="{{ URL::to('kategori/satu') }}" class="text-decoration-none"> -->
+            <p class="card-text"><h2 class="text-center">Latest Fleet</h2><br>
+            <h5 class="text-justify">You don't have to worry about getting an old car! 
+              The rental cars that we provide are cars with very good conditions that will make you comfortable during the trip.</h5></p>
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- kategori kedua -->
+    <div class="col-md-4">
+      <div class="card mb-4 shadow-sm">
+        <!-- <a href="{{ URL::to('kategori/dua') }}">
+          <img src="{{asset('images/slide1.jpg') }}" alt="foto kategori" class="card-img-top"> -->
+        </a>
+        <div class="card-body">
+          <!-- <a href="{{ URL::to('kategori/dua') }}" class="text-decoration-none"> -->
+            <p class="card-text"><h2 class="text-center">Competitive price!</h2><br>
+<h5 class="text-justify">Junior Rentcar Pekanbaru offers competitive prices, 
+good service quality and provides a fleet that is regularly maintained and insured in excellent condition.</h5></p>
+          </a>
+        </div>
+      </div>
+    </div>
+    <!-- kategori ketiga -->
+    <div class="col-md-4">
+      <div class="card mb-4 shadow-sm">
+        <!-- <a href="{{ URL::to('kategori/tiga') }}">
+          <img src="{{asset('images/slide1.jpg') }}" alt="foto kategori" class="card-img-top"> -->
+        <!-- </a> -->
+        <div class="card-body">
+          <!-- <a href="{{ URL::to('kategori/tiga') }}" class="text-decoration-none"></a> -->
+            <p class="card-text"><h2 class="text-center">Experienced</h2><br>
+              <h5 class="text-justify">Need-A-Car is owned and managed by people who have long experience in the field of car rental services. 
+              For more than Decades we attach great importance to the quality of the services provided.</h5></p>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end kategori produk -->
     </div>
   </div>
 </div>
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
   <div class="container">
-    <a class="navbar-brand" href="/">RENTAL MOBIL</a>
+    <a class="navbar-brand" href="/">NEED-A-CAR</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -8,13 +8,13 @@
       <ul class="mr-auto navbar-nav"></ul>
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="{{ url('/') }}">BERANDA</a>
+          <a class="nav-link" href="{{ url('/') }}">HOMEPAGE</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ URL::to('login') }}">LOGIN</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="{{ URL::to('registrasi') }}">REGISTRASI</a>
+          <a class="nav-link" href="{{ URL::to('registrasi') }}">REGISTRATION</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="{{ URL::to('about') }}">ABOUT US</a>
